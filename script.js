1cda826c52ce077c9d45d9f06fd32fbaec7ce581
FRACEtcg <eugene.opalihin@gmail.com>
2022-05-13 19:57:44 +0300

    edited footer, fixed arr funcs and DOM variables

--- script.js
+++ script.js
@@ -3,19 +3,33 @@ const paper = document.getElementById('paper');
 const scissors = document.getElementById('scissors');
 const play = document.getElementById('play');
 const choice = ['rock', 'paper', 'scissors'];
-rock.addEventListener('click', round());
-paper.addEventListener('click', round());
-scissors.addEventListener('click', round());
-play.addEventListener('click', game())
+let firstblock = document.querySelector('.text');
+let lastblock = document.querySelector('.text2');
+rock.addEventListener('click', () => {
+	yourChoice(this);
+	round();
+});
+paper.addEventListener('click', () => {
+	yourChoice(this);
+	round();
+});
+scissors.addEventListener('click', () => {
+	yourChoice(this);
+	round();
+});
+play.addEventListener('click', () =>game());
+
+function yourChoice(name) {
+	playerchoice = name.id;
+}
+yourChoice(rock);
 function opponentsChoice() {
-  thechoice = choice[Math.floor(Math.random()*choice.length)];
+	thechoice = choice[Math.floor(Math.random() * choice.length)];
 }
 function game() {
-  play.style.display = 'none';
-  let firstblock = document.body.querySelector('text');
-  let lastblock = document.body.querySelectorAll('text').pop();
-  play.style.display = 'block';
+	play.style.display = 'none';
+	firstblock.textContent = `Get ready for the game of ROCK, PAPER, SCISSORS against the computer.
+  Choose your option by clicking the appropriate picture below`;
 }
 function round() {
-
 }
